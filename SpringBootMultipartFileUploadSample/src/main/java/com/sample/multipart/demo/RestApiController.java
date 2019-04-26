231b9ea2df1e260a0d4a1fb6ff4cf4cecef191ee
Ahn Sung-dae <limecode.v@gmail.com>
2019-04-26 16:51:00 +0900

    Update RestApiController.java
    Http 응답 코드 추가.

--- SpringBootMultipartFileUploadSample/src/main/java/com/sample/multipart/demo/RestApiController.java
+++ SpringBootMultipartFileUploadSample/src/main/java/com/sample/multipart/demo/RestApiController.java
@@ -2,9 +2,12 @@ package com.sample.multipart.demo;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -18,13 +21,14 @@ import javax.servlet.http.HttpServletRequest;
 @RestController
 public class RestApiController {
     private static final Logger logger = LoggerFactory.getLogger(RestApiController.class);
-    private static final String FILE_HOME ="/Users/limecode/temp/files";
+    private static final String FILE_HOME ="/Users/limecode/temp/2019Suwon/files";
 
     @RequestMapping("/uploadEvent")
-    public String uploadEvent(HttpServletRequest servletRequest,
-                                  @ModelAttribute MultipartVO multipartVO,
-                                  Model model) {
-        multipartVO.getData();
+    @ResponseBody
+    public ResponseEntity uploadEvent(HttpServletRequest servletRequest,
+                                              @ModelAttribute MultipartVO multipartVO,
+                                              Model model) {
+        logger.info("MultipartVO: " + multipartVO.getData());
 
         if (null != multipartVO.getFL01()) saveFile(multipartVO.getFL01());
         if (null != multipartVO.getFL02()) saveFile(multipartVO.getFL02());
@@ -34,7 +38,10 @@ public class RestApiController {
         if (null != multipartVO.getFL06()) saveFile(multipartVO.getFL06());
         if (null != multipartVO.getFL07()) saveFile(multipartVO.getFL07());
 
-        return multipartVO.getData();
+
+//        return multipartVO.getData();
+//        return ResponseEntity.ok(multipartVO.getData());
+        return ResponseEntity.status(HttpStatus.OK).body("{\"message\":\"정상\"}");
     }
 
     void saveFile(MultipartFile mfile){
