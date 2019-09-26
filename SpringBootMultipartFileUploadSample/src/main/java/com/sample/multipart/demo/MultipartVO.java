65b899ef3ec480c036cba743a3d6f50e931c5417
meerip <limecode.v@gmail.com>
2019-09-26 17:50:34 +0900

    Update MultipartVO.java

--- SpringBootMultipartFileUploadSample/src/main/java/com/sample/multipart/demo/MultipartVO.java
+++ SpringBootMultipartFileUploadSample/src/main/java/com/sample/multipart/demo/MultipartVO.java
@@ -3,6 +3,12 @@ package com.sample.multipart.demo;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.Serializable;
+import java.util.List;
+
+import javax.validation.constraints.NotNull;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.Serializable;
 
 import javax.validation.constraints.NotNull;
 
@@ -15,12 +21,14 @@ public class MultipartVO implements Serializable {
 //    private List<MultipartFile> files;
 //    private MultipartFile files;
     private MultipartFile FL01;
-    private MultipartFile FL02;
-    private MultipartFile FL03;
-    private MultipartFile FL04;
-    private MultipartFile FL05;
-    private MultipartFile FL06;
-    private MultipartFile FL07;
+    private List<MultipartFile> FL02;
+    private List<MultipartFile> FL04;
+//    private MultipartFile FL02;
+//    private MultipartFile FL03;
+//    private MultipartFile FL04;
+//    private MultipartFile FL05;
+//    private MultipartFile FL06;
+//    private MultipartFile FL07;
 
     public String getData() {
         return data;
@@ -31,6 +39,10 @@ public class MultipartVO implements Serializable {
     }
 
 
+    public static long getSerialVersionUID() {
+        return serialVersionUID;
+    }
+
     public MultipartFile getFL01() {
         return FL01;
     }
@@ -39,51 +51,19 @@ public class MultipartVO implements Serializable {
         this.FL01 = FL01;
     }
 
-    public MultipartFile getFL02() {
+    public List<MultipartFile> getFL02() {
         return FL02;
     }
 
-    public void setFL02(MultipartFile FL02) {
+    public void setFL02(List<MultipartFile> FL02) {
         this.FL02 = FL02;
     }
 
-    public MultipartFile getFL03() {
-        return FL03;
-    }
-
-    public void setFL03(MultipartFile FL03) {
-        this.FL03 = FL03;
-    }
-
-    public MultipartFile getFL04() {
+    public List<MultipartFile> getFL04() {
         return FL04;
     }
 
-    public void setFL04(MultipartFile FL04) {
+    public void setFL04(List<MultipartFile> FL04) {
         this.FL04 = FL04;
-    }
-
-    public MultipartFile getFL05() {
-        return FL05;
-    }
-
-    public void setFL05(MultipartFile FL05) {
-        this.FL05 = FL05;
-    }
-
-    public MultipartFile getFL06() {
-        return FL06;
-    }
-
-    public void setFL06(MultipartFile FL06) {
-        this.FL06 = FL06;
-    }
-
-    public MultipartFile getFL07() {
-        return FL07;
-    }
-
-    public void setFL07(MultipartFile FL07) {
-        this.FL07 = FL07;
     }
 }
